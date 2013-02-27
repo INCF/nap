8decaea58d048d45a9d2172449e41a9381bb320a
richstoner <stonerri@gmail.com>
2013-02-26 23:11:30 -0800

    adding python query tools

---
+++ SectionImage.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class SectionImage(object):
+    """docstring for SectionImage"""
+
+    def __init__(self, _name):
+        self.name =  _name
+        print 'Initialized as %s SectionImage' % (self.name)
+        
+
+
